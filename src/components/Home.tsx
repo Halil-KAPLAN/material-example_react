03e20449e61650a7b60a487b64c4f2c2a6110bc8
Halil KAPLAN <halil-kaplan@windowslive.com>
2024-02-29 04:13:21 +0300

    changed home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,30 @@
+import { Box, Link, Stack } from "@mui/material";
+import { styled } from "@mui/system";
+
+const StyledLink = styled(Link)({
+  textDecoration: "none",
+  "&:hover": {
+    color: "blue",
+  },
+});
+
 const Home = () => {
-  return <div>Material UI Components</div>;
+  return (
+    <Stack>
+      <img src="https://cdn-media-1.freecodecamp.org/images/1*FDNeKIUeUnf0XdqHmi7nsw.png"></img>
+
+      <Box sx={{ margin: "20px auto" }}>
+        <StyledLink
+          variant="body1"
+          href="https://mui.com/material-ui/getting-started/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Go Material UI
+        </StyledLink>
+      </Box>
+    </Stack>
+  );
 };
 
 export default Home;
